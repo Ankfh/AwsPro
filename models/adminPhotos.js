8dca42c9d5fc1734ff533ad89e57a2262e851f8a
Taimoor Badar <taimoor@labd.tech>
2023-02-14 13:44:23 +0500

    admin photos schema

---
+++ models/adminPhotos.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema;
+
+const photosSchema = new schema(
+  {
+    productId: {
+      type: String,
+      required: true,
+    },
+    companyId: {
+      type: String,
+    },
+    photos: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("adminphotos", photosSchema);
